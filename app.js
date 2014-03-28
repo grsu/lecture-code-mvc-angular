73e219290af782a5925e31336c850864e09773f0
Pavel Zubkou <pavel.zubkou@gmail.com>
2014-03-28 11:52:43 +0300

    Вынести список студентов в директиву studentsList (код с лекции 28 марта)

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 angular.module('для-старосты', [])
-  .controller('StudentsListCtrl', function ($scope, $window, $location) {
-    $scope.students = [
-      { name: 'Вася' },
-      { name: 'Дима' },
-      { name: 'Вася 2' },
-      { name: 'Вася 4' },
-      { name: 'Катя' },
-      { name: 'Федя' }
-    ];
+  .controller('StudentsListCtrl', function ($scope, $window, $location, $http) {
+    $scope.students = [];
+
+    var studentsUrl = 'http://localhost:3000/students';
+
+    $http.get(studentsUrl)
+      .success(function (data) {
+        $scope.students = data;
+      });
 
     $scope.addStudent = function () {
       if (!$scope.firstName) {
         return;
       }
 
-      $scope.students.push({ name: $scope.firstName });
+      var data = { name: $scope.firstName, active: true };
+
+      $scope.students.push(data);
       $scope.firstName = '';
+
+      $http.post(studentsUrl, data)
+        .success(function () {
+          console.log('saved to database');
+        })
     };
 
     console.log($location, $window);
+  })
+  .directive('studentsList', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/students-list.html',
+      scope: {
+        data: '=',
+        title: '@'
+      },
+      controller: function ($scope) {
+        $scope.reportStudent = function (student) {
+          student.active = false;
+          student.removed = true;
+          student.reasonForRemoval = 'жалоба от старосты';
+        }
+      }
+    };
   });
 
 angular.module('для-деканата', [])
